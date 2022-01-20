43005c90169ad870d7802c165687e3d16b6f3213
tony-1302 <tohirbek.otaqulov1302@mail.ru>
2022-01-20 11:39:35 +0500

    dars  12

---
+++ dars_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 10:29:38 2022
+
+@author: Tohir
+"""
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+
+print('\n', 5**4)
+print('\n', 22 % 4)
+
+
---
+++ if → else.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 09:51:52 2022
+
+@author: Tohir
+
+"""
+
+yosh = int(input('Yoshingiz → '))
+
+if yosh > 40 :
+    print("Xush kelibsz otaxon!!!")
+elif yosh > 20 :
+    print("Xush kelibsz do`stim")
+else :
+    print("Uzr kirish mumkin emas !!!")
+    
